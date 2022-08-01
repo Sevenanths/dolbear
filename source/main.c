dfb37d6504b608f48a8831eb89830a76cd6d3332
Sevenanths <anthernate@gmail.com>
2022-08-01 16:59:55 +0200

    Rework game into a single struct

--- source/main.c
+++ source/main.c
@@ -45,7 +45,7 @@ enum BearDirection {
 struct BearObject {
 	int type;
 	int direction;
-	int x;
+	int x;game
 	int y;
 };
 
@@ -59,6 +59,16 @@ enum BearObjectDirection {
 enum BearObjectTypes {
 	FIRE = 1,
 	STAR = 2
+};
+
+enum GameModes {
+	TITLE, GAME, GAME_OVER
+};
+
+struct Game {
+	struct Bear bear;
+	struct BearObject objects[NUM_OBJECTS * 2];
+	int score;
 };
 
 int random_integer(int minimum_number, int max_number) {
@@ -79,6 +89,38 @@ int random_direction() {
 
 int score = 0;
 
+void init_game(struct Game *game) {
+	/* 
+    	Bear
+    */
+
+    // Set bear coordinates to the middle of the screen
+    game->bear.x = (GC_WIDTH / 2) - (OBJECT_WIDTH / 2);
+    game->bear.y = (GC_HEIGHT / 2) - (OBJECT_HEIGHT / 2);
+
+    /*
+		Objects
+    */
+    /* Set the first NUM_OBJECTS objects to fire */
+    for (int i = 0; i < NUM_OBJECTS; ++i)
+    {
+    	game->objects[i].type = FIRE;
+    }
+    /* Set the first NUM_OBJECTS objects to fire */
+    for (int i = NUM_OBJECTS; i < 2 * NUM_OBJECTS; ++i)
+    {
+    	game->objects[i].type = STAR;
+    }
+
+    /* Generate random coordinates and directions for all objects */
+    for (int i = 0; i < NUM_OBJECTS * 2; ++i)
+    {
+    	game->objects[i].x = random_coordinate_x();
+    	game->objects[i].y = random_coordinate_y();
+    	game->objects[i].direction = random_direction();
+    }
+}
+
 int main(int argc, char **argv) {
     // Initialise the Graphics & Video subsystem
     GRRLIB_Init();
@@ -94,36 +136,9 @@ int main(int argc, char **argv) {
 
     GRRLIB_ttfFont *fnt_score = GRRLIB_LoadTTF(dinbekbold_ttf, dinbekbold_ttf_size);
 
-    /* 
-    	Bear
-    */
-    struct Bear *obj_bear = malloc(sizeof(struct Bear));
-    // Set bear coordinates to the middle of the screen
-    obj_bear->x = (GC_WIDTH / 2) - (OBJECT_WIDTH / 2);
-    obj_bear->y = (GC_HEIGHT / 2) - (OBJECT_HEIGHT / 2);
+	struct Game* game = malloc(sizeof(struct Game));
 
-    /*
-		Objects
-    */
-    struct BearObject objects[NUM_OBJECTS * 2];
-    /* Set the first NUM_OBJECTS objects to fire */
-    for (int i = 0; i < NUM_OBJECTS; ++i)
-    {
-    	objects[i].type = FIRE;
-    }
-    /* Set the first NUM_OBJECTS objects to fire */
-    for (int i = NUM_OBJECTS; i < 2 * NUM_OBJECTS; ++i)
-    {
-    	objects[i].type = STAR;
-    }
-
-    /* Generate random coordinates and directions for all objects */
-    for (int i = 0; i < NUM_OBJECTS * 2; ++i)
-    {
-    	objects[i].x = random_coordinate_x();
-    	objects[i].y = random_coordinate_y();
-    	objects[i].direction = random_direction();
-    }
+    init_game(game);   
 
     // Loop forever
     while(1) {
@@ -138,46 +153,46 @@ int main(int argc, char **argv) {
         if (PAD_ButtonsDown(0) & PAD_BUTTON_START)  break;
 
         if (PAD_StickY(0) > 18) {
-			obj_bear->direction = BEAR_UP;
+			game->bear.direction = BEAR_UP;
 		}
 		if (PAD_StickY(0) < -18) {
-			obj_bear->direction = BEAR_DOWN;
+			game->bear.direction = BEAR_DOWN;
 		}
 		if (PAD_StickX(0) > 18) {
-			obj_bear->direction = BEAR_RIGHT;
+			game->bear.direction = BEAR_RIGHT;
 		}
 		if (PAD_StickX(0) < -18) {
-			obj_bear->direction = BEAR_LEFT;
+			game->bear.direction = BEAR_LEFT;
 		}
 
 		/* 
 			Bear collision (walls)
 		*/
-		if (obj_bear->x - BEAR_SPEED <= OBJECT_HEIGHT) {
-			obj_bear->direction = BEAR_RIGHT;
-		} else if (obj_bear->x + BEAR_SPEED >= GC_WIDTH - (OBJECT_WIDTH * 2)) {
-			obj_bear->direction = BEAR_LEFT;
-		} else if (obj_bear->y - BEAR_SPEED <= OBJECT_HEIGHT) {
-			obj_bear->direction = BEAR_DOWN;
-		} else if (obj_bear->y + BEAR_SPEED >= GC_HEIGHT - (OBJECT_HEIGHT * 2)) {
-			obj_bear->direction = BEAR_UP;
+		if (game->bear.x - BEAR_SPEED <= OBJECT_HEIGHT) {
+			game->bear.direction = BEAR_RIGHT;
+		} else if (game->bear.x + BEAR_SPEED >= GC_WIDTH - (OBJECT_WIDTH * 2)) {
+			game->bear.direction = BEAR_LEFT;
+		} else if (game->bear.y - BEAR_SPEED <= OBJECT_HEIGHT) {
+			game->bear.direction = BEAR_DOWN;
+		} else if (game->bear.y + BEAR_SPEED >= GC_HEIGHT - (OBJECT_HEIGHT * 2)) {
+			game->bear.direction = BEAR_UP;
 		}
 
 		/*
 			Bear movement
 		*/
-		switch(obj_bear->direction) {
+		switch(game->bear.direction) {
 			case BEAR_UP:
-				obj_bear->y -= BEAR_SPEED;
+				game->bear.y -= BEAR_SPEED;
 				break;
 			case BEAR_DOWN:
-				obj_bear->y += BEAR_SPEED;
+				game->bear.y += BEAR_SPEED;
 				break;
 			case BEAR_LEFT:
-				obj_bear->x -= BEAR_SPEED;
+				game->bear.x -= BEAR_SPEED;
 				break;
 			case BEAR_RIGHT:
-				obj_bear->x += BEAR_SPEED;
+				game->bear.x += BEAR_SPEED;
 				break;
 		}
 
@@ -187,47 +202,47 @@ int main(int argc, char **argv) {
 		for (int i = 0; i < NUM_OBJECTS * 2; ++i)
 		{
 			// Left wall collision
-			if (objects[i].x - OBJECT_SPEED <= OBJECT_WIDTH) {
-				if (objects[i].direction == UP_LEFT) {
-					objects[i].direction = UP_RIGHT;
-				} else if (objects[i].direction == DOWN_LEFT) {
-					objects[i].direction = DOWN_RIGHT;
+			if (game->objects[i].x - OBJECT_SPEED <= OBJECT_WIDTH) {
+				if (game->objects[i].direction == UP_LEFT) {
+					game->objects[i].direction = UP_RIGHT;
+				} else if (game->objects[i].direction == DOWN_LEFT) {
+					game->objects[i].direction = DOWN_RIGHT;
 				}
 			// Right wall collision
-			} else if (objects[i].x + OBJECT_SPEED >= GC_WIDTH - (OBJECT_WIDTH * 2)) {
-				if (objects[i].direction == UP_RIGHT) {
-					objects[i].direction = UP_LEFT;
-				} else if (objects[i].direction == DOWN_RIGHT) {
-					objects[i].direction = DOWN_LEFT;
+			} else if (game->objects[i].x + OBJECT_SPEED >= GC_WIDTH - (OBJECT_WIDTH * 2)) {
+				if (game->objects[i].direction == UP_RIGHT) {
+					game->objects[i].direction = UP_LEFT;
+				} else if (game->objects[i].direction == DOWN_RIGHT) {
+					game->objects[i].direction = DOWN_LEFT;
 				}
 			// Top wall collision
-			} else if (objects[i].y - OBJECT_SPEED <= OBJECT_HEIGHT) {
-				if (objects[i].direction == UP_LEFT) {
-					objects[i].direction = DOWN_LEFT;
-				} else if (objects[i].direction == UP_RIGHT) {
-					objects[i].direction = DOWN_RIGHT;
+			} else if (game->objects[i].y - OBJECT_SPEED <= OBJECT_HEIGHT) {
+				if (game->objects[i].direction == UP_LEFT) {
+					game->objects[i].direction = DOWN_LEFT;
+				} else if (game->objects[i].direction == UP_RIGHT) {
+					game->objects[i].direction = DOWN_RIGHT;
 				}
 			// Bottom wall
-			} else if (objects[i].y + OBJECT_SPEED >= GC_HEIGHT - (OBJECT_HEIGHT * 2)) {
-				if (objects[i].direction == DOWN_LEFT) {
-					objects[i].direction = UP_LEFT;
-				} else if (objects[i].direction == DOWN_RIGHT) {
-					objects[i].direction = UP_RIGHT;
+			} else if (game->objects[i].y + OBJECT_SPEED >= GC_HEIGHT - (OBJECT_HEIGHT * 2)) {
+				if (game->objects[i].direction == DOWN_LEFT) {
+					game->objects[i].direction = UP_LEFT;
+				} else if (game->objects[i].direction == DOWN_RIGHT) {
+					game->objects[i].direction = UP_RIGHT;
 				}
 			}
 
-			if (objects[i].direction == UP_LEFT) {
-				objects[i].x -= OBJECT_SPEED;
-				objects[i].y -= OBJECT_SPEED;
-			} else if (objects[i].direction == UP_RIGHT) {
-				objects[i].x += OBJECT_SPEED;
-				objects[i].y -= OBJECT_SPEED;
-			} else if (objects[i].direction == DOWN_LEFT) {
-				objects[i].x -= OBJECT_SPEED;
-				objects[i].y += OBJECT_SPEED;
-			} else if (objects[i].direction == DOWN_RIGHT) {
-				objects[i].x += OBJECT_SPEED;
-				objects[i].y += OBJECT_SPEED;
+			if (game->objects[i].direction == UP_LEFT) {
+				game->objects[i].x -= OBJECT_SPEED;
+				game->objects[i].y -= OBJECT_SPEED;
+			} else if (game->objects[i].direction == UP_RIGHT) {
+				game->objects[i].x += OBJECT_SPEED;
+				game->objects[i].y -= OBJECT_SPEED;
+			} else if (game->objects[i].direction == DOWN_LEFT) {
+				game->objects[i].x -= OBJECT_SPEED;
+				game->objects[i].y += OBJECT_SPEED;
+			} else if (game->objects[i].direction == DOWN_RIGHT) {
+				game->objects[i].x += OBJECT_SPEED;
+				game->objects[i].y += OBJECT_SPEED;
 			}
 		}
 
@@ -238,18 +253,18 @@ int main(int argc, char **argv) {
 		{
 			// All hail the mighty bounding box calculation 
 			// I ported this from my LUA code in 2015! It might just work!
-			if (obj_bear->x < objects[i].x + OBJECT_WIDTH &&
-				obj_bear->x + OBJECT_WIDTH > objects[i].x &&
-				obj_bear->y < objects[i].y + OBJECT_HEIGHT &&
-				OBJECT_HEIGHT + obj_bear->y > objects[i].y) {
+			if (game->bear.x < game->objects[i].x + OBJECT_WIDTH &&
+				game->bear.x + OBJECT_WIDTH > game->objects[i].x &&
+				game->bear.y < game->objects[i].y + OBJECT_HEIGHT &&
+				OBJECT_HEIGHT + game->bear.y > game->objects[i].y) {
 
-				objects[i].x = random_coordinate_x();
-				objects[i].y = random_coordinate_y();
-				objects[i].direction = random_direction();
+				game->objects[i].x = random_coordinate_x();
+				game->objects[i].y = random_coordinate_y();
+				game->objects[i].direction = random_direction();
 
-				if (objects[i].type == FIRE) {
+				if (game->objects[i].type == FIRE) {
 					score -= 1000;
-				} else if (objects[i].type == STAR) {
+				} else if (game->objects[i].type == STAR) {
 					score += 1000;
 				}
 			}
@@ -283,15 +298,15 @@ int main(int argc, char **argv) {
 		}
 
 		// Draw bear
-		GRRLIB_DrawImg(obj_bear->x, obj_bear->y, spr_bear, 0, 1, 1, GRRLIB_WHITE);
+		GRRLIB_DrawImg(game->bear.x, game->bear.y, spr_bear, 0, 1, 1, GRRLIB_WHITE);
 
 		// Draw objects
 		for (int i = 0; i < NUM_OBJECTS * 2; ++i)
 		{
-			if (objects[i].type == FIRE) {
-				GRRLIB_DrawImg(objects[i].x, objects[i].y, spr_fire, 0, 1, 1, GRRLIB_WHITE);
-			} else if (objects[i].type == STAR) {
-				GRRLIB_DrawImg(objects[i].x, objects[i].y, spr_star, 0, 1, 1, GRRLIB_WHITE);
+			if (game->objects[i].type == FIRE) {
+				GRRLIB_DrawImg(game->objects[i].x, game->objects[i].y, spr_fire, 0, 1, 1, GRRLIB_WHITE);
+			} else if (game->objects[i].type == STAR) {
+				GRRLIB_DrawImg(game->objects[i].x, game->objects[i].y, spr_star, 0, 1, 1, GRRLIB_WHITE);
 			}
 		}
 
