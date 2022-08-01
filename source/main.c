0bc193516880b12b5a6edc5f2b24fd6b41cef611
Sevenanths <anthernate@gmail.com>
2022-08-01 12:05:31 +0200

    Implement a GameCube cursor

--- source/main.c
+++ source/main.c
@@ -10,7 +10,35 @@ static GXRModeObj *rmode = NULL;
 
 void *Initialise();
 
-int test = 0;
+int cursor_x = 300;
+int cursor_y = 250;
+
+void DrawHLine(int x1, int x2, int y, int color) {
+    int i;
+    y = 320 * y;
+    x1 >>= 1;
+    x2 >>= 1;
+    for (i = x1; i <= x2; i++) {
+        u32 *tmpfb = xfb;
+        tmpfb[y+i] = color;
+    }
+}
+
+void DrawVLine(int x, int y1, int y2, int color) {
+    int i;
+    x >>= 1;
+    for (i = y1; i <= y2; i++) {
+        u32 *tmpfb = xfb;
+        tmpfb[x + (640 * i) / 2] = color;
+    }
+}
+
+void DrawBox(int x1, int y1, int x2, int y2, int color) {
+    DrawHLine (x1, x2, y1, color);
+    DrawHLine (x1, x2, y2, color);
+    DrawVLine (x1, y1, y2, color);
+    DrawVLine (x2, y1, y2, color);
+}
 
 int main(int argc, char **argv) {
 
@@ -31,7 +59,7 @@ int main(int argc, char **argv) {
 		int buttonsDown = PAD_ButtonsDown(0);
 		
 		if( buttonsDown & PAD_BUTTON_A ) {
-			printf("Button A pressed.\n");
+			//printf("Button A pressed.\n");
 		}
 
 		if (buttonsDown & PAD_BUTTON_START) {
@@ -40,20 +68,32 @@ int main(int argc, char **argv) {
 
 		// Analog movement
 		if (PAD_StickY(0) > 18) {
-			printf("Joystick moved up.\n");
+			//printf("Joystick moved up.\n");
+			cursor_y -= 20;
 		}
 
 		if (PAD_StickY(0) < -18) {
-			printf("Joystick moved down\n");
+			//printf("Joystick moved down\n");
+			cursor_y += 20;
 		}
 
 		if (PAD_StickX(0) > 18) {
-			printf("Joystick moved right.\n");
+			//printf("Joystick moved right.\n");
+			cursor_x += 20;
 		}
 
 		if (PAD_StickX(0) < -18) {
-			printf("Joystick moved left\n");
+			//printf("Joystick moved left\n");
+			cursor_x -= 20;
 		}
+
+		// Clear the framebuffer to avoid "Mario Kart 8 test track" vibes
+		VIDEO_ClearFrameBuffer(rmode, xfb, COLOR_BLACK);
+
+		DrawBox (cursor_x, cursor_y, cursor_x + 1, cursor_y + 1, COLOR_WHITE);
+
+		// Vsync, all good 👍
+		VIDEO_WaitVSync();
 	}
 
 	return 0;
